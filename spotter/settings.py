06b8730ee3f5a9d03ad96126b611116c4f8868e8
barani11kumar-ai <barani11kumar@gmail.com>
2026-05-14 21:50:50 +0530

    Changed Static and add openai on requirement.txt

--- spotter/settings.py
+++ spotter/settings.py
@@ -149,7 +149,8 @@ LOGIN_REDIRECT_URL = 'index'
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS=(os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static'),]
 
 # The URL used to access media in the browser
 MEDIA_URL = '/media/'
