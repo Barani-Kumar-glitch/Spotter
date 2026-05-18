1af244a3fb2157b25b635e70b521e08887e957ac
barani11kumar-ai <barani11kumar@gmail.com>
2026-05-18 20:10:15 +0530

    Host Changing

--- spotter/settings.py
+++ spotter/settings.py
@@ -48,11 +48,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv(
-    "ALLOWED_HOSTS",
-    "127.0.0.1,localhost,spotter-eemr.onrender.com"
-).split(",")
+# ALLOWED_HOSTS = os.getenv(
+#     "ALLOWED_HOSTS",
+#     "127.0.0.1,localhost,spotter-eemr.onrender.com"
+# ).split(",")
 
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
