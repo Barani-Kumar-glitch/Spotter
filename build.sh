dce3d83423bae28979cb16294979cb353a4edba8
Barani Kumar S <boss002racer@gmail.com>
2026-06-09 22:19:57 +0530

    Fix build script by removing duplicate command
    Removed duplicate collectstatic command and added migrate.

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-set -o errexit
 
 pip install -r requirements.txt
-<<<<<<< HEAD
+
 python manage.py collectstatic --noinput
-=======
-python manage.py collectstatic --noinput
->>>>>>> 21c92cddff268d23a9899c592f2ef665f0de7285
+
+python manage.py migrate
